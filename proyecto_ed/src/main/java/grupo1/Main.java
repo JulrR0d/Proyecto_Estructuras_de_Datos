68bb420cd7dc3ca8f32850fbed241eda51af5655
Carlos Romero <cromerosi@unal.edu.co>
2026-04-02 21:17:24 -0500

    Comentario: Idea inicial vs Idea implementada. Carlos

--- proyecto_ed/src/main/java/grupo1/Main.java
+++ proyecto_ed/src/main/java/grupo1/Main.java
@@ -4,6 +4,18 @@ import grupo1.Clases.Paciente;
 import grupo1.Estructuras.ColaTriage;
 
 public class Main {
+    /*
+    Carlos: Mi idea principalmente era realizar una lista simple, que añadiera los pacientes por triage, y en caso de empate de triage, añadirlos al final por hora, pero, que tuviera un puntero a cada triage, para no tener que recorrer la lista cada vez que se añada un paciente, y así mantener el orden de prioridad. Ya luego preguntandole a GPT, me dijo que lo mejor era usar una cola de prioridad:
+        GPT:
+            🎯 Conclusión directa
+                ✔️ Sí, se puede
+                ✔️ Es interesante (más “ingeniería”)
+                ⚠️ Pero:
+                👉 No es la más óptima en Big O puro
+                👉 Es más compleja de implementar correctamente
+    Entonces teniendo en cuenta que lo primordial es la eficiencia, y la complejidad, GPT me recomendó usar una cola de prioridad y buckets, que es lo que finalmente implementé. La idea es tener 5 listas enlazadas (una por cada nivel de triage), y cada lista mantiene el orden FIFO de llegada. Para atender, se busca desde el nivel 1 hasta el 5, lo que es O(1) porque el número de niveles de triage es fijo y pequeño. 
+    */
+
     public static void main(String[] args) {
         // Cola principal de triage por niveles (1..5).
         ColaTriage colaTriage = new ColaTriage();
