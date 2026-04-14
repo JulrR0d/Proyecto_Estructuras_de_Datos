d1d5d83c6657812d24b205d84d718b7ff048f4ab
diego251307 <2513edb@gmail.com>
2026-04-14 14:38:09 -0500

    Update Paciente.java atributos, getters y setters
    Añadi atributos extra (edad, EPS, sexo, sintomas) para tener mas datos del paciente y asignarle un nivel de triage correcto, ademas de las getters y setters par poder editarlo con ciertar restricciones en algunos para tener un rango de evaluo correcto

--- proyecto_ed/src/main/java/grupo1/Clases/Paciente.java
+++ proyecto_ed/src/main/java/grupo1/Clases/Paciente.java
@@ -11,14 +11,22 @@ public class Paciente {
     // Atributos
     private long id;
     private String nombre;
+    private int edad;
+    private char sexo; // m/f
+    private String EPS;
+    private String sintomas;
     private LocalDate fechaIngreso;
     private LocalTime horaIngreso;
     private Byte nivelTriage;
 
     // Constructor
-    public Paciente(long id, String nombre, Byte nivelTriage) {
+    public Paciente(long id, String nombre, int edad, char sexo, String EPS, String sintomas, Byte nivelTriage) {
         this.id = id;
         this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.EPS = EPS;
+        this.sintomas = sintomas;
         this.fechaIngreso = LocalDate.now();
         this.horaIngreso = LocalTime.now();
         validarNivelTriage(nivelTriage);
@@ -32,6 +40,18 @@ public class Paciente {
     public String getNombre() {
         return nombre;
     }
+    public int getEdad() {
+        return edad;
+    }
+    public char getSexo() {
+        return sexo;
+    }
+    public String getEPS(){
+        retunr EPS;
+    }
+    public String getsintomas(){
+        return sintomas;
+    }
     public LocalDate getFechaIngreso() {
         return fechaIngreso;
     }
@@ -43,11 +63,33 @@ public class Paciente {
     }
 
     // Métodos Set
-    public void setId(long id) {
+    public void setId(long id) { //pdoriamso eliminar este pq igual es mejor no editar el id si es la manera en la que vamos a clasificarlos para el arbol
         this.id = id;
     }
     public void setNombre(String nombre) {
+        if (nombre  == null || nombre.trim().isEmpty()){
+            throw new RunTimeException("Debes ingresar un nombre.");
+        }
         this.nombre = nombre;
+    }
+    public void setEdad(int edad) {
+        if (edad < 0 || edad > 100) {
+            throw new RunTimeException("Edad invalida.");
+        }
+        this.edad = edad;
+    }
+    public void setSexo(char sexo) {
+        if (sexo == 'M' || sexo == 'F') {
+            this.sexo = sexo;
+        } else {
+            throw new RunTimeException("Sexo invalido (M o F)");
+        }
+    }
+    public void setEps(String eps) {
+        this.eps = eps;
+    }
+    public void setSintomas(String sintomas) {
+        this.sintomas = sintomas;
     }
     public void setFechaIngreso(LocalDate fechaIngreso) {
         this.fechaIngreso = fechaIngreso;
@@ -71,12 +113,14 @@ public class Paciente {
 
     @Override
     public String toString() {
-        return "Paciente{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                ", nivelTriage=" + nivelTriage +
-                ", fechaIngreso=" + fechaIngreso +
-                ", horaIngreso=" + horaIngreso +
+        return "Paciente {" +
+                "id =" + id +
+                ", nombre ='" + nombre + '\'' +
+                ", edad = " + edad + 
+                ", EPS = " + EPS +
+                ", nivelTriage =" + nivelTriage +
+                ", fechaIngreso =" + fechaIngreso + ", a las " + horaIngreso +
+                ", sintomas =" + sintomas +
                 '}';
     }
     
